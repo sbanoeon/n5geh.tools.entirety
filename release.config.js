417f68bbc513f8f3c1d5ba64b0121de4f24bbde1
Saira Bano <saira.bano@eonerc.rwth-aachen.de>
2024-03-11 11:16:45 +0100

    feat(devices): check semantics release

--- release.config.js
+++ release.config.js
@@ -16,6 +16,14 @@ module.exports = {
                 "parserOpts": {
                     "noteKeywords": ["BREAKING CHANGE", "BREAKING CHANGES", "BREAKING"]
                 },
+                "presetConfig": {
+                      types: [
+                        { type: "feat", section: "Featuressss", hidden: false },
+                        { type: "fix", section: "Bug Fixes", hidden: false },
+                        { type: "docs", section: "Miscellaneous Chores", hidden: false },
+                        { type: "chore", section: "Miscellaneous Chores", hidden: false },
+                      ],
+                    },
                 "writerOpts": {
                     "groupBy": "scope"
                     // "commitsSort": ["subject", "scope"]
