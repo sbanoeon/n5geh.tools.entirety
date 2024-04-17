30995fb02c7a5fff6fa7ffe98a0b586810930d55
Saira Bano <saira.bano@eonerc.rwth-aachen.de>
2024-04-17 10:43:06 +0200

    feat(new): check semantics release BREAKING CHANGE: test

--- release.config.js
+++ release.config.js
@@ -1,34 +1,60 @@
-module.exports = {
-    "plugins": [
-        [
-            "@semantic-release/commit-analyzer",
-            {
-                "preset": "conventionalcommits",
-                "parserOpts": {
-                    "noteKeywords": ["BREAKING CHANGE", "BREAKING CHANGES", "BREAKING"]
+module.exports =
+    {
+        "plugins": [
+            [
+                "@semantic-release/commit-analyzer",
+                {
+                    "preset": "angular",
+                    "parserOpts": {
+                        "noteKeywords": ["BREAKING CHANGE", "BREAKING CHANGES", "BREAKING"]
+                    }
                 }
-            }
-        ],
-        [
-            "@semantic-release/release-notes-generator",
-            {
-                "preset": "conventionalcommits",
-                "parserOpts": {
-                    "noteKeywords": ["BREAKING CHANGE", "BREAKING CHANGES", "BREAKING"]
-                },
-                "presetConfig": {
-                      types: [
-                        { type: "feat", section: "Featuressss", hidden: false },
-                        { type: "fix", section: "Bug Fixes", hidden: false },
-                        { type: "docs", section: "Miscellaneous Chores", hidden: false },
-                        { type: "chore", section: "Miscellaneous Chores", hidden: false },
-                      ],
+            ],
+            [
+                "@semantic-release/release-notes-generator",
+                {
+                    "preset": "angular",
+                    "parserOpts": {
+                        "noteKeywords": ["BREAKING CHANGE", "BREAKING CHANGES", "BREAKING"]
                     },
-                // "writerOpts": {
-                //     "groupBy": "scope",
-                //     "linkReferences": "False",
-                // "commitsSort": ["subject", "sco
-            }
+                    "writerOpts": {
+                        "commitsSort": ["subject", "scope"]
+                    }
+                }
+            ]
         ]
-    ]
-}
+    }
+// {
+//     "plugins": [
+//         [
+//             "@semantic-release/commit-analyzer",
+//             {
+//                 "preset": "conventionalcommits",
+//                 "parserOpts": {
+//                     "noteKeywords": ["BREAKING CHANGE", "BREAKING CHANGES", "BREAKING"]
+//                 }
+//             }
+//         ],
+//         [
+//             "@semantic-release/release-notes-generator",
+//             {
+//                 "preset": "conventionalcommits",
+//                 "parserOpts": {
+//                     "noteKeywords": ["BREAKING CHANGE", "BREAKING CHANGES", "BREAKING"]
+//                 },
+//                 "presetConfig": {
+//                     types: [
+//                         {type: "feat", section: "Featuressss", hidden: false},
+//                         {type: "fix", section: "Bug Fixes", hidden: false},
+//                         {type: "docs", section: "Miscellaneous Chores", hidden: false},
+//                         {type: "chore", section: "Miscellaneous Chores", hidden: false},
+//                     ],
+//                 },
+//                 // "writerOpts": {
+//                 //     "groupBy": "scope",
+//                 //     "linkReferences": "False",
+//                 // "commitsSort": ["subject", "sco
+//             }
+//         ]
+//     ]
+// }
