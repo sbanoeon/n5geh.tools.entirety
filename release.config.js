371e4ba6bc7ea8d371873e5d62433fb80a0ca251
Saira Bano <saira.bano@eonerc.rwth-aachen.de>
2024-04-17 13:45:23 +0200

    feat(entities): check semantics release again BREAKING CHANGE: test

--- release.config.js
+++ release.config.js
@@ -29,7 +29,7 @@ module.exports =
                     },
                     "writerOpts": {
                         "groupBy": "scope",
-                        //"commitsSort": ["subject", "scope"]
+                        "commitsSort": ["type", "scope"]
                     }
                 }
             ],
