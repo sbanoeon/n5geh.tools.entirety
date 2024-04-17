25a4cdc3e9b78d29a1d2d118fc6118b3dbd5a93c
Saira Bano <saira.bano@eonerc.rwth-aachen.de>
2024-04-17 09:05:09 +0200

    feat(semantics): check semantics release BREAKING CHANGE: test

--- release.config.js
+++ release.config.js
@@ -3,7 +3,7 @@ module.exports = {
         [
             "@semantic-release/commit-analyzer",
             {
-                "preset": "conventionalcommits",
+                "preset": "angular",
                 "parserOpts": {
                     "noteKeywords": ["BREAKING CHANGE", "BREAKING CHANGES", "BREAKING"]
                 }
